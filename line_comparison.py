aa4cd066b70d4f0864f3bd4dc90ce30e473616c4
Amit Kumar Yadav <96034453+00Amit0@users.noreply.github.com>
2022-09-12 17:06:18 +0530

    Update line_comparison.py

--- line_comparison.py
+++ line_comparison.py
@@ -25,27 +25,6 @@ class Line:
             logging.warning("Recheck the code")
             logging.exception(e)
 
-    def line_equality(self, l2):
-        """
-        Function to compare the lengths of the line
-        :param l2:
-        :return:
-        """
-        logging.debug("Method running")
-        try:
-            print(self.length_of_line())
-            length1 = self.length_of_line()
-            length2 = l2.length_of_line()
-            if length1 == length2:
-                print("both lines are equal ")
-            elif length1 > length2:
-                print("Line 1 is greater ")
-            else:
-                print("Line 2 is greater")
-        except Exception as e:
-            logging.warning("Recheck the code")
-            logging.exception(e)
-
 
 if __name__ == "__main__":
     x1 = int(input("Enter value for first point on X axis : "))
@@ -55,11 +34,5 @@ if __name__ == "__main__":
     l1 = Line({'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2})
     length1 = l1.length_of_line()
     print(length1)
-    x1 = int(input("Enter value for first point on X axis : "))
-    x2 = int(input("Enter value for second point on X axis : "))
-    y1 = int(input("Enter value for first point on Y axis : "))
-    y2 = int(input("Enter value for second point on Y axis : "))
-    l2 = Line({'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2})
-    length2 = l2.length_of_line()
-    print(length2)
-    l1.line_equality(l2)
+
+   
